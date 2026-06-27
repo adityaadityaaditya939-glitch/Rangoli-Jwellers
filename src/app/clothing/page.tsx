be45c8f85b1d3fa9dbdb42d7f037250eb899b536
Aditya Sharma <adityaadityaaditya939@gmail.com>
2026-06-27 20:13:26 +0530

    correction

--- src/app/clothing/page.tsx
+++ src/app/clothing/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import ClothingPageClient from "./ClothingPageClient";
 
 export default function ClothingPage() {
-  return <ClothingPageClient />;
+  return (
+    <Suspense fallback={<div className="mx-auto max-w-7xl px-4 py-8 lg:px-8">Loading...</div>}>
+      <ClothingPageClient />
+    </Suspense>
+  );
 }
