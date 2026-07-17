0f520fcb002f7ce300a413f2dfef3438def883a2
Aditya Sharma <adityaadityaaditya939@gmail.com>
2026-07-17 15:05:01 +0530

    Fix UploadThing import and remove unused import

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,7 +1,7 @@
-import { createRouteHandlerClient } from "uploadthing/next";
+import { createRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
 
-export const { GET, POST } = createRouteHandlerClient({
+export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
 });
