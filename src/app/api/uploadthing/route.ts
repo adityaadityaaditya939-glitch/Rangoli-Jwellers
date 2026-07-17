d1efcec5d10313060c6652e2587877274c29f1a8
Aditya Sharma <adityaadityaaditya939@gmail.com>
2026-07-18 01:44:06 +0530

    Switch to custom upload route for local development

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -4,8 +4,4 @@ import { ourFileRouter } from "./core";
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-  config: {
-    uploadthingId: process.env.UPLOADTHING_APP_ID,
-    uploadthingSecret: process.env.UPLOADTHING_SECRET,
-  },
 });
